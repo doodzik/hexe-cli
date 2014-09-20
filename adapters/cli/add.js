ac98ae2376401163bbcc98bd1dbb7661846873c7
doodzik <4004blog@gmail.com>
2014-09-20 13:27:48 +0200

    fix #2 && make add work

--- adapters/cli/add.js
+++ adapters/cli/add.js
@@ -3,31 +3,30 @@ var generator = require('../../lib/generator');
 var fs = require('fs');
 
 module.exports = function addCmd(argv, hexeObj){
-//TODO [-as new_contract_name] [-v/--version 0.0.0 ]
+  //TODO [-as new_contract_name] [-v/--version 0.0.0 ]
   return when([
     argv._[0],
-    (argv._[1] || hexeObj.service)
+    (argv._[1] || hexeObj.service),
+    hexeObj.lang
   ])
-  .then(function(contract, adapter){
-    var gSet = generator.set(lang+'_'+contract);
-
+  .spread(function(contract, adapter, lang){
+    var gSet = generator.set(lang+'-'+contract);
+    hexeObj.contracts = hexeObj.contracts || {}
+    generator.exists(gSet(''))
     if(!(contract in hexeObj.contracts)){
       hexeObj.contracts[contract] = {};
-      return gSet('contract')
+      generator.run(gSet('contract'))
+      generator.run(gSet('adapter') + ' ' + adapter)
     }else{
       var file = process.cwd()+'/adapters/'+contract+'/'+adapter
       fs.exists(file, function(exists) {
         if (exists) {
           throw new Error('adapter already defiend')
         }else{
-          return gSet('adapter')
+          generator.run(gSet('adapter') + ' ' + adapter)
         }
       })
     }
-  })
-  .then(function(_generator){
-    generator.exists(_generator)
-    generator.run(_generator);
   })
   .then(function(){
     return hexeObj;
